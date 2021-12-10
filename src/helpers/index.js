be18712bfa2be8acf4c60a510e54ddc4fc891a4b
ale_figure8 <gomeznieto@gmail.com>
2021-12-10 19:55:59 -0300

    Adding Date, Currency functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,16 @@ export function generateId(){
     const date = Date.now().toString(36)
     return random + date
 }
+
+export function formatCurrency (currency){
+    return currency.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    })
+}
+
+export function formatDate(date){
+    const newDate = new Date(date)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return newDate.toDateString('en-US', options)
+}
